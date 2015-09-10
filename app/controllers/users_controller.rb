4025553be61f93e990894531b93da241788e45ab
takero.yasuhara <takero.yasuhara@litalico.co.jp>
2015-09-10 09:54:37 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Our Shopping Site!!"
+      redirect_to root_url # サインインしている状態で商品一覧ページへリダイレクト.
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
